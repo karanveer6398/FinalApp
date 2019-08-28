dfeb09174c6886dd3a364ef1efa151428dbce77f
Karanveer <sharmak911@yahoo.com>
2019-08-28 14:11:03 +1000

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { conditionalExpression } from '@babel/types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { username: "", password: "" }
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    }
+    handleChalange(source) {
+        this.setState({ username: source.target.value })
+    }
+    handleChalange(source) {
+        this.setState({ password: source.target.value })
+    }
+    render() {
+        return (
+            <div class="container">
+                <div class="card card-container">
+
+                    <img id="profile-img" className="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+                    <p id="profile-name" className="profile-name-card"></p>
+                    <form class="form-signin">
+                        <span id="reauth-email" className="reauth-email"></span>
+                        <label htmlFor="username">Email:</label>
+                        <input type="email"  onChange={this.handleUsernameChange} value={this.state.username} id="username" className="form-control" autofocus />
+                        <label htmlFor="password">Password:</label>
+                        <input type="password"  onChange={this.handlePasswordChange} value={this.state.password} id="password" className="form-control" required ></input>
+                        <div id="remember" className="checkbox">
+                            <label>
+                                <input type="checkbox" value="remember-me" /> Remember me
+                            </label>
+                        </div>
+                        <button id="click" name="click" onClick={this.handleclick}>Sign in</button>
+                    </form>
+                    <a href="#" className="forgot-password">
+                        Forgot the password?
+
+                    </a>
+                </div>
+            </div>
+
+        )
+    }
 }
 
 export default App;
